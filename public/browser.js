9e037d09ba53148461d41b790a297eb54e212c0e
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-06-13 17:30:16 +0100

    my code working now

--- public/browser.js
+++ public/browser.js
@@ -1,87 +1,66 @@
 function itemTemplate(item) {
-  return `
-  <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
   <span class="item-text">${item.text}</span>
   <div>
-    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+  <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
   </div>
-</li>
-  
-  `;
+  </li>`
 }
 
-// create features
-let createField = document.getElementById("create-field");
+// Initial Page Load Render
+let ourHTML = items
+  .map(function (item) {
+    return itemTemplate(item)
+  })
+  .join("")
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
+// Create Feature
+let createField = document.getElementById("create-field")
 
 document.getElementById("create-form").addEventListener("submit", function (e) {
-  e.preventDefault();
+  e.preventDefault()
   axios
-    .post("/create-item", {
-      text: createField.value,
-    })
+    .post("/create-item", { text: createField.value })
     .then(function (response) {
-      //    create the html for a new item
-      document
-        .getElementById("item-list")
-        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-      createField.value = "";
-      createField.focus();
+      // Create the HTML for a new item
+      document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+      createField.value = ""
+      createField.focus()
     })
-
-    //   Created the html fo new page
     .catch(function () {
-      console.log("please try again later");
-    });
-});
+      console.log("Please try again later.")
+    })
+})
 
 document.addEventListener("click", function (e) {
-  // Delete features
+  // Delete Feature
   if (e.target.classList.contains("delete-me")) {
-    if (confirm("Do you want to delete")) {
+    if (confirm("Do you really want to delete this item permanently?")) {
       axios
-        .post("/delete-item", {
-          id: e.target.getAttribute("data-id"),
-        })
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
         .then(function () {
-          e.target.parentElement.parentElement.remove();
+          e.target.parentElement.parentElement.remove()
         })
         .catch(function () {
-          console.log("please try again later");
-        });
+          console.log("Please try again later.")
+        })
     }
   }
 
-  // INITIAL PAGE LOAD RENDER
-
-  let ourHTML = items
-    .map(function (item) {
-      return itemTemplate(item);
-    })
-    .join("");
-
-  document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML);
-
-  //update features
+  // Update Feature
   if (e.target.classList.contains("edit-me")) {
-    let userInput = prompt(
-      "Enter your desired new text",
-      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
-    );
+    let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
     if (userInput) {
       axios
-        .post("/update-item", {
-          text: userInput,
-          id: e.target.getAttribute("data-id"),
-        })
+        .post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") })
         .then(function () {
-          e.target.parentElement.parentElement.querySelector(
-            ".item-text"
-          ).innerHTML = userInput;
+          e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
         })
         .catch(function () {
-          console.log("please try again later");
-        });
+          console.log("Please try again later.")
+        })
     }
   }
-});
+})
--- server.js
+++ server.js
@@ -1,89 +1,75 @@
-let express = require("express");
-let { MongoClient, ObjectId } = require("mongodb");
+let express = require("express")
+let { MongoClient, ObjectId } = require("mongodb")
 
-let app = express();
-let db;
+let app = express()
+let db
 
-app.use(express.static(`public`));
+app.use(express.static("public"))
 
 async function go() {
   let client = new MongoClient(
     "mongodb+srv://kene:nweke081@cluster0.xc7hoie.mongodb.net/TodoApp?retryWrites=true&w=majority"
   );
-  await client.connect();
-  db = client.db();
-  app.listen(5000);
+
+  await client.connect()
+  db = client.db()
+  app.listen(5000)
 }
 
-go();
+go()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", async function (req, res) {
-  const items = await db.collection("items").find().toArray();
-
-  res.send(`
-    
-    <!DOCTYPE html>
-<html>
-<head>
+  const items = await db.collection("items").find().toArray()
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simple To-Do App</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-</head>
-<body>
+  </head>
+  <body>
   <div class="container">
-    <h1 class="display-4 text-center py-1">To-Do App</h1>
-    
-    <div class="jumbotron p-3 shadow-sm">
-      <form id="create-form"  action='/create-item' method='POST'>
-        <div class="d-flex align-items-center">
-          <input id="create-field"  name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-          <button class="btn btn-primary">Add New Item</button>
-        </div>
-      </form>
-    </div>
-    
-    <ul id="item-list" class="list-group pb-5">
+  <h1 class="display-4 text-center py-1">To-Do App!</h1>
   
-    </ul>
-    
+  <div class="jumbotron p-3 shadow-sm">
+  <form id="create-form" action="/create-item" method="POST">
+  <div class="d-flex align-items-center">
+  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+  <button class="btn btn-primary">Add New Item</button>
+  </div>
+  </form>
+  </div>
+  
+  <ul id="item-list" class="list-group pb-5">
+  </ul>
+  
   </div>
 
   <script>
   let items = ${JSON.stringify(items)}
   </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
-  <script src="/browser.js">
- 
-  </script>
   
-</body>
-</html>
-    
-    `);
-});
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+  </body>
+  </html>`)
+})
+
 app.post("/create-item", async function (req, res) {
-  const info = await db.collection("items").insertOne({ text: req.body.text });
-  // console.log(req.body.item);
-  // res.send("Thanks for Submitting this form ");
-  res.json({ _id: info.insertedId, text: req.body.text });
-});
+  const info = await db.collection("items").insertOne({ text: req.body.text })
+  res.json({ _id: info.insertedId, text: req.body.text })
+})
 
 app.post("/update-item", async function (req, res) {
-  await db
-    .collection("items")
-    .findOneAndUpdate(
-      { _id: new ObjectId(req.body.id) },
-      { $set: { text: req.body.text } }
-    );
-  res.send("Success");
-});
+  await db.collection("items").findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: req.body.text } })
+  res.send("Success")
+})
 
 app.post("/delete-item", async function (req, res) {
-  await db.collection("items").deleteOne({ _id: new ObjectId(req.body.id) });
-  res.send("Success");
-});
+  await db.collection("items").deleteOne({ _id: new ObjectId(req.body.id) })
+  res.send("Success")
+})
